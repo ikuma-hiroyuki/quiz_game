952e849c90dfa4f1e8af0105579958310d6a94a4
ikuma-hiroyuki <chemicalbondo@gmail.com>
2022-07-14 06:38:05 +0900

    クイズ変数の作成

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from data import question_data
 from question_model import Question
+from quiz_brain import QuizBrain
 
 question_bank = []
 
 for question in question_data:
     new_question = Question(question["text"], question["answer"])
     question_bank.append(new_question)
+
+quiz = QuizBrain(question_bank)
+quiz.next_question()
